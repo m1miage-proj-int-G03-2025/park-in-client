1a3f13e56aa2c72a6493ec9b9cfa450be7ce67ae
NadineYasser1 <nadineyasser92@gmail.com>
2025-01-23 17:59:21 +0100

    Add axios configs

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,8 +1,13 @@
 'use client'
 import {HeroUIProvider} from "@heroui/react"
 import  AuthProvider  from './AuthProvider'
+import { useEffect } from "react"
+import { setupInterceptor} from "@/configs/axios"
 
 export function Providers({children}: { children: React.ReactNode }) {
+  useEffect(() => {
+    setupInterceptor()
+  }, [])
   return (
     <HeroUIProvider>
       <AuthProvider>
