36f84b68889f2157914eeb7d6b199daf3ceeef25
Coulibaly N'Djo-Soro <couliban@im2ag-202-15.u-ga.fr>
2025-01-30 10:42:50 +0100

    perf: ameliorer processus reservation

--- src/app/common/providers/ReservationDataProvider.tsx
+++ src/app/common/providers/ReservationDataProvider.tsx
@@ -1,14 +1,29 @@
 "use client";
-import { useContext, useState, ReactNode } from "react";
+import { useContext, useState, ReactNode, useEffect } from "react";
 import { CreateReservationParams } from "../types/create-reservation-params";
 import { ReservationDataContext, ReservationDataContextType } from "../contexts/reservationDataContext";
 
-export default function ReservationDataProvider({ children}: { children: ReactNode }) {
-    const [reservationData, setReservationData] = useState<CreateReservationParams | null>(null);
+export default function ReservationDataProvider({ children }: { children: ReactNode }) {
+    const [reservationData, setReservationData] = useState<CreateReservationParams | null>(() => {
+        if (typeof window !== 'undefined') {
+            const storedReservationData = localStorage.getItem('reservationData');
+            return storedReservationData ? JSON.parse(storedReservationData) : null;
+        }
+    });
 
     const saveReservationData = (data: CreateReservationParams | null) => {
         setReservationData(data);
+        if (data) {
+            localStorage.setItem('reservationData', JSON.stringify(data));
+        } else {
+            localStorage.removeItem('reservationData');
+        }
     }
+
+    useEffect(() => {
+        const storedReservationData = localStorage.getItem('reservationData');
+        setReservationData(storedReservationData ? JSON.parse(storedReservationData) : null);
+    }, []);
 
     return (
         <ReservationDataContext.Provider value={{ reservationData, saveReservationData }}>
